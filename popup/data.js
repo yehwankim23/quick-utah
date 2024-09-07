1d56f65b0cd9437144c0f304dd90cc5a990e8122
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2024-09-08 06:36:15 +0900

    Fix Class Schedule

--- popup/data.js
+++ popup/data.js
@@ -10,7 +10,7 @@ const data = [
     {
       name: "Class Schedule",
       icon: "../images/class-schedule.png",
-      url: "https://student.apps.utah.edu/uofu/stu/ClassSchedules/uac/1248/index.html",
+      url: "https://class-schedule.app.utah.edu/uac/1248/index.html",
     },
     { name: "Umail", icon: "../images/umail.png", url: "https://outlook.office365.com/mail" },
   ],
